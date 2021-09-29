bb008c8fdd37c41e723ec7058e45e6463bb64e80
Dilara <dilaraismn@gmail.com>
2021-09-29 12:55:34 +0300

    12. reading touch input

---
+++ Assets/Scripts/BallHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BallHandler : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (!Touchscreen.current.primaryTouch.press.isPressed)
+        {
+            return;
+        }
+        Vector2 touchPositon = Touchscreen.current.primaryTouch.position.ReadValue();
+        Debug.Log(touchPositon);
+    }
+}
