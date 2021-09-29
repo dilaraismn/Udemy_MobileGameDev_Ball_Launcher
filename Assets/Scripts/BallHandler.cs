490abd8096e0f817e1bf75f6758dd710aa0c09f0
Dilara <dilaraismn@gmail.com>
2021-09-29 14:03:47 +0300

    15. launching the ball

--- Assets/Scripts/BallHandler.cs
+++ Assets/Scripts/BallHandler.cs
@@ -5,8 +5,12 @@ using UnityEngine.InputSystem;
 
 public class BallHandler : MonoBehaviour
 {
-    private Camera mainCamera; //to convert to world space 
     [SerializeField] private Rigidbody2D currentBallRb;
+    [SerializeField] private SpringJoint2D currentBallSj;
+    [SerializeField] private float detachDelay;
+
+    private Camera mainCamera; //to convert to world space 
+    private bool isDraggin;
     void Start()
     {
         mainCamera = Camera.main;
@@ -14,12 +18,20 @@ public class BallHandler : MonoBehaviour
 
     void Update()
     {
+        if(currentBallRb == null) return;
+        
         if (!Touchscreen.current.primaryTouch.press.isPressed)
         {
-            currentBallRb.isKinematic = false;
+            if (isDraggin)
+            {
+                LaunchBall();
+            }
+
+            isDraggin = false;
             return;
         }
-        
+
+        isDraggin = true;
         currentBallRb.isKinematic = true;
 
         Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
@@ -29,6 +41,20 @@ public class BallHandler : MonoBehaviour
 
         currentBallRb.position = worldPosition; 
         //when we only use this line ball goes crazy because it's position is also effected by the gravity and the spring joint
+    }
 
+    private void LaunchBall()
+    {
+        currentBallRb.isKinematic = false;
+        currentBallRb = null;
+        
+        //Invoke("DetachBall", detachDelay); 
+        Invoke(nameof(DetachBall), detachDelay); 
+    }
+
+    private void DetachBall()
+    {
+        currentBallSj.enabled = false;
+        currentBallSj = null;
     }
 }
