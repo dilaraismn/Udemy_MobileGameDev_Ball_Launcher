8e076baa336f7d2781badd5e9c18ee6bd4c42181
Dilara <dilaraismn@gmail.com>
2021-09-29 13:28:02 +0300

    14. move objects with touch

--- Assets/Scripts/BallHandler.cs
+++ Assets/Scripts/BallHandler.cs
@@ -6,6 +6,7 @@ using UnityEngine.InputSystem;
 public class BallHandler : MonoBehaviour
 {
     private Camera mainCamera; //to convert to world space 
+    [SerializeField] private Rigidbody2D currentBallRb;
     void Start()
     {
         mainCamera = Camera.main;
@@ -15,13 +16,19 @@ public class BallHandler : MonoBehaviour
     {
         if (!Touchscreen.current.primaryTouch.press.isPressed)
         {
+            currentBallRb.isKinematic = false;
             return;
         }
+        
+        currentBallRb.isKinematic = true;
+
         Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
 
         Vector3 worldPosition = mainCamera.ScreenToWorldPoint(touchPosition);
-        //even tho it's a vector 3 it will 0 out the Z cordinents and make it as a vector 2 value
-        
-        Debug.Log(worldPosition);
+        //even tho it's a vector 3 it will 0 out the Z coordinates and make it as a vector 2 value
+
+        currentBallRb.position = worldPosition; 
+        //when we only use this line ball goes crazy because it's position is also effected by the gravity and the spring joint
+
     }
 }
